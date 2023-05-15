13881953e59fd5750ad3fdb69d6f80c8dd33360d
Sj-ackdo <84838763+Sj-ackdo@users.noreply.github.com>
2023-05-15 23:49:05 +0200

    Add files via upload

---
+++ TowerOfHanoi.py
@@ -0,0 +1,11 @@
+#a way of determining the minimum amount of moves depending on n = disks.
+# 2^n - 1 = min amount of moves
+# Fun fact: This formula is exactly the same as Mersenne's number 
+
+amountOfMoves = int(0)
+
+print('How many disks are there?')
+answerDisks = int(input())
+amountOfMoves = 2 ** answerDisks - 1
+
+print('if there are', answerDisks,'disks, then the minimal amount of moves are', amountOfMoves)
